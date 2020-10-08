591150b728511805c037fc2bf12022a0bec9dfa6
Andrew Martin <hello@andrewmart.in>
2020-10-08 13:38:37 -0700

    updating contentful embedded

--- src/util/contentful.ts
+++ src/util/contentful.ts
@@ -1,6 +1,10 @@
 import { Entry, EntryCollection } from 'contentful'
 import { documentToHtmlString, Options } from '@contentful/rich-text-html-renderer'
-import { BLOCKS } from '@contentful/rich-text-types'
+import { BLOCKS, INLINES } from '@contentful/rich-text-types'
+
+enum CONTENT_TYPES {
+  LINKED_IMAGE = 'linkedImage',
+}
 
 interface IContentfulItem {
   fields: any
@@ -10,11 +14,25 @@ const renderEmbeddedPageContent = (node: any) => {
   return documentToHtmlString(node?.data?.target?.fields?.page?.fields?.body)
 }
 
+const renderEmbeddedEntryContent = (node: any) => {
+  const type = node?.data?.target?.sys?.contentType?.sys?.id
+  const link = node?.data?.target?.fields?.link
+  const src = node?.data.target.fields.image.fields.file.url
+
+  switch (type) {
+    case CONTENT_TYPES.LINKED_IMAGE:
+      return `<a href='${link}'><img src='${src}' /></a>`
+    default:
+      return ''
+  }
+}
+
 const options: Options = {
   renderNode: {
-    [BLOCKS.EMBEDDED_ENTRY]: (node) => renderEmbeddedPageContent(node),
+    [BLOCKS.EMBEDDED_ENTRY]: renderEmbeddedPageContent,
     [BLOCKS.EMBEDDED_ASSET]: (node) =>
       `<custom-component>${JSON.stringify(node)}</custom-component>`,
+    [INLINES.EMBEDDED_ENTRY]: renderEmbeddedEntryContent,
   },
 }
 
